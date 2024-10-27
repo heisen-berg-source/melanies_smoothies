d6f8a1d9836cf475a984044f5653288fa6e7f082
heisen-berg-source <justin.dungog81@gmail.com>
2024-10-27 12:03:07 +0800

    Update streamlit_app.py
    if loop on the ingredients lists json line 51 - 60

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,15 @@ if ingredients_list:
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+if ingredients _list:
+      ingredients_string = ''
+
+      for fruit_chosen in ingredients_list:
+            ingredients_string += fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            # st.text(fruityvice_response.json())
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
+
