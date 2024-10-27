38b45ff953f83c1f5e05208ac9f1431b27203172
heisen-berg-source <justin.dungog81@gmail.com>
2024-10-27 12:59:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if ingredients_list:
             st.subheader(fruit_chosen + 'Nutrition Information')
             # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
             # st.text(fruityvice_response.json())
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+            # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
