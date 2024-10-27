7b714e8279cfa390fbfbef54ac3a3795a7e8aa8e
heisen-berg-source <justin.dungog81@gmail.com>
2024-10-27 11:18:27 +0800

    Update streamlit_app.py
    respelled typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
-      """Choose the fruits you want in yohur Smoothie!"""
+      """Choose the fruits you want in your Smoothie!"""
 )
 
 
